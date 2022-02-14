be7c6bad8f5c8c9a978f2204f2ca73ddcf1720d1
Tek <tek@inbox.ru>
2022-02-14 22:15:39 +0400

    added base

--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -1,6 +1,7 @@
 import { StoryFn as StoryFunction, StoryContext } from "@storybook/addons";
 import { useEffect, useGlobals } from "@storybook/addons";
-
+import { FpsMeter } from 'frame-rate-meter/dist';
+let fps: FpsMeter = null;
 export const withGlobals = (StoryFn: StoryFunction, context: StoryContext) => {
   const [{ myAddon }] = useGlobals();
   // Is the addon being used in the docs panel
@@ -24,6 +25,12 @@ export const withGlobals = (StoryFn: StoryFunction, context: StoryContext) => {
 
 function displayToolState(selector: string, state: any) {
   const rootElement = document.querySelector(selector);
+  if (state.myAddon && !fps) {
+    fps = new FpsMeter({ theme: 'dark', position: 'topRight', maxFps: 60 });
+  } else if (fps !== null) {
+    fps.destroy();
+    fps = null;
+  }
   let preElement = rootElement.querySelector("pre");
 
   if (!preElement) {
@@ -38,7 +45,7 @@ function displayToolState(selector: string, state: any) {
 
   preElement.innerText = `This snippet is injected by the withGlobals decorator.
 It updates as the user interacts with the ⚡ tool in the toolbar above.
-
+<FpsView/>
 ${JSON.stringify(state, null, 2)}
 `;
 }
